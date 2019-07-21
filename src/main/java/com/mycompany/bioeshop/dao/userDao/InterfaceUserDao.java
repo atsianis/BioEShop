d73a2f1f0e3f15c07b571faa0f7486e8f88e52d5
Thanos Tsianis <thanos95_ts@hotmail.com>
2019-07-21 14:54:44 +0300

    Interface UserDao added

---
+++ src/main/java/com/mycompany/bioeshop/dao/userDao/InterfaceUserDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.bioeshop.dao.userDao;
+
+/**
+ *
+ * @author samsung np350
+ */
+public class InterfaceUserDao {
+    
+}
