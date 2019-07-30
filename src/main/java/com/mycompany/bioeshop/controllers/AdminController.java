144e9ebaa7a004753f978641d6d6503f76097347
GiorgosVal <giorgos.valamats@gmail.com>
2019-07-30 23:44:00 +0300

    fixed some bugs

--- src/main/java/com/mycompany/bioeshop/controllers/AdminController.java
+++ src/main/java/com/mycompany/bioeshop/controllers/AdminController.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.bioeshop.controllers;
 
 import com.mycompany.bioeshop.dao.CustomerDao;
@@ -27,95 +26,88 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    
+
     @Autowired
     ProductDao pdao;
-    
+
     @Autowired
     CustomerDao cdao;
-    
-    @Autowired 
+
+    @Autowired
     CustomerService customerService;
-    
+
     @RequestMapping(value = {"/products/edit/{id}"}, method = RequestMethod.GET)
-    public String editProduct(ModelMap model,@PathVariable String id){
+    public String editProduct(ModelMap model, @PathVariable String id) {
         Product p = pdao.getProductById(Integer.parseInt(id));
 //        Hibernate.initialize(p.getOrderdetailsList());
-        model.addAttribute("edit",true);
+        model.addAttribute("edit", true);
         model.addAttribute("p", p);
-        model.addAttribute("act","Edit");
+        model.addAttribute("act", "Edit");
         return "saveeditproduct";
-        
+
     }
-    
+
     @RequestMapping(value = {"/products/create"}, method = RequestMethod.GET)
-    public String newProduct(ModelMap model){
+    public String newProduct(ModelMap model) {
         Product p = new Product();
 //        Hibernate.initialize(p.getOrderdetailsList());
-        model.addAttribute("edit",false);
+        model.addAttribute("edit", false);
         model.addAttribute("p", p);
-        model.addAttribute("act","Create");
+        model.addAttribute("act", "Create");
         return "saveeditproduct";
-        
+
     }
-    
+
     @RequestMapping(value = {"/products/save"}, method = RequestMethod.POST)
     public String saveProduct(@Valid Product p, BindingResult result,
-            ModelMap model){
-        
+            ModelMap model) {
+
         if (result.hasErrors()) {
-            model.addAttribute("message","There was an error trying to save. Please try again");
-            return "redirect:/products";
+            //model.addAttribute("message", "There was an error trying to save. Please try again");
+            return "redirect:/products/";
         }
         
-        boolean done = false;
-        if(p.getProductId() == null){
-            done = pdao.addProduct(p);
-        }else{
-            done = pdao.updateProduct(p);
+        if ((p.getProductId() == 0) && (pdao.addProduct(p))) {
+               // model.addAttribute("message", "Entry done");
+        } else if (pdao.updateProduct(p)) {
+            return "redirect:/products/" + p.getProductId();
         }
-        // pithana minimata pou mporoun na emfanizontai kapou kapote. Twra de ta xeirizomai
-        if (done) {
-            model.addAttribute("message","Entry done");
-        }else{
-            model.addAttribute("message","There was a problem");
-        }
-        model.addAttribute("products", pdao.getAllProducts());
-        return "products";
+
+        //model.addAttribute("message", "There was a problem");
+        return "redirect:/products/";
     }
-    
+
     @RequestMapping(value = {"/products/delete/{id}"}, method = RequestMethod.GET)
-    public String deleteProduct(ModelMap model,@PathVariable int id){
-        boolean deleted = pdao.deleteProductById(id);
-        if (deleted){
-            model.addAttribute("message","Product deleted successfully");
-        }else{
-            model.addAttribute("message","Something went wrong. Could not delete");
+    public String deleteProduct(ModelMap model, @PathVariable int id) {
+        if (pdao.deleteProductById(id)) {
+            model.addAttribute("message", "Product deleted successfully");
+        } else {
+            model.addAttribute("message", "Something went wrong. Could not delete");
         }
-        model.addAttribute("products", pdao.getAllProducts());
-        return "products";
+        //model.addAttribute("products", pdao.getAllProducts());
+        return "redirect:/products/";
     }
-    
+
     @RequestMapping(value = {"/customers"}, method = RequestMethod.GET)
-    public String allCustomers(ModelMap model){
+    public String allCustomers(ModelMap model) {
         Product p = new Product();
 //        Hibernate.initialize(p.getOrderdetailsList());
         List<Customer> customers = cdao.getAllRegisteredCustomers();
-        model.addAttribute("customers",customers);
+        model.addAttribute("customers", customers);
         return "customers";
-        
+
     }
-    
+
     @RequestMapping(value = {"/customers/update/{id}"}, method = RequestMethod.GET)
-    public String editCustomer(ModelMap model,@PathVariable int id){
+    public String editCustomer(ModelMap model, @PathVariable int id) {
         Customer c = cdao.getCustomerById(id);
-        model.addAttribute("customer",c);
-        model.addAttribute("action","/BioEShop/admin/customers/save");
-        model.addAttribute("cancel","BioEShop/admin/customers");
+        model.addAttribute("customer", c);
+        model.addAttribute("action", "/BioEShop/admin/customers/save");
+        model.addAttribute("cancel", "BioEShop/admin/customers");
         return "updateprofile";
-        
+
     }
-    
+
     @RequestMapping(value = {"/customers/save"}, method = RequestMethod.POST)
     public String saveCustomer(@Valid Customer customer, BindingResult result,
             ModelMap model, @RequestParam("oldemail") String oldemail) {
@@ -123,10 +115,6 @@ public class AdminController {
         if (result.hasErrors()) {
             return "updateprofile";
         }
-        
-    
-    
-   
 
         if (!oldemail.equals(customer.getEmail())) {
             if (!customerService.isEmailUnique(customer.getCustomerId(), customer.getEmail())) {
@@ -135,7 +123,6 @@ public class AdminController {
                 return "updateprofile";
             }
         }
-        
 
         if (customerService.updateCustomer(customer)) {
             model.addAttribute("message", "Info was updated successfully.");
@@ -143,32 +130,30 @@ public class AdminController {
             model.addAttribute("success", "Info was not updated.");
         }
         List<Customer> customers = cdao.getAllRegisteredCustomers();
-        model.addAttribute("customers",customers);
+        model.addAttribute("customers", customers);
         return "customers";
     }
-    
+
     @RequestMapping(value = {"/customers/delete/{id}"}, method = RequestMethod.GET)
-    public String deleteCustomer(ModelMap model,@PathVariable int id){
+    public String deleteCustomer(ModelMap model, @PathVariable int id) {
         boolean deleted = cdao.deleteCustomerById(id);
-        if (deleted){
-            model.addAttribute("message","Customer deleted successfully");
-        }else{
-            model.addAttribute("message","Could not delete customer");
+        if (deleted) {
+            model.addAttribute("message", "Customer deleted successfully");
+        } else {
+            model.addAttribute("message", "Could not delete customer");
         }
         List<Customer> customers = cdao.getAllRegisteredCustomers();
-        model.addAttribute("customers",customers);
+        model.addAttribute("customers", customers);
         return "customers";
-        
+
     }
-    
-    @RequestMapping (value = {"/orders/pending"}, method = RequestMethod.GET)
-    public String pendingOrders(ModelMap model){
-        
-        
+
+    @RequestMapping(value = {"/orders/pending"}, method = RequestMethod.GET)
+    public String pendingOrders(ModelMap model) {
+
         return "pendingorders";
     }
-    
-    
+
     private String getPrincipal() {
         String userName = null;
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -180,5 +165,5 @@ public class AdminController {
         }
         return userName;
     }
-    
+
 }
--- src/main/webapp/WEB-INF/views/customers.jsp
+++ src/main/webapp/WEB-INF/views/customers.jsp
@@ -8,7 +8,7 @@
         <table border="1" id="example" class="stripe order-column hover cell-border display myTable" style="width:100%">
             <tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Address</th><th>Phone Number</th><th>Update</th><th>Delete</th></tr>
             <c:forEach var="c" items="${customers}">
-                <tr><td>${c.fname}</td><td>${c.lname}</td><td>${c.email}</td><td>${c.address}</td><td>${c.phoneNumber}</td><td><a href="customers/update/${c.customerId}">Edit</a></td><td><a href="/BioEShop/admin/customers/delete/${c.customerId}">Delete</a></td></tr>
+                <tr><td>${c.fname}</td><td>${c.lname}</td><td>${c.email}</td><td>${c.address}</td><td>${c.phoneNumber}</td><td><a href="/BioEShop/admin/customers/update/${c.customerId}">Edit</a></td><td><a href="/BioEShop/admin/customers/delete/${c.customerId}">Delete</a></td></tr>
             </c:forEach>
         </table>
         <p>${message}</p><br>
